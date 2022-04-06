a35518cc7acbeab198c87db01c877cb991204577
rrajput22 <102693224+rrajput22@users.noreply.github.com>
2022-04-05 23:00:49 -0600

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The average of those numbers is: ", (num_1 + num_2 + num_3 + num_4 + num_5) / 5)
